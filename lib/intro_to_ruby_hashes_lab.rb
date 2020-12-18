dab8bcbf3fd0db44cc2d5976a714fde971fa5b76
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-18 21:07:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,12 +4,12 @@ def my_hash_creator(key, value)
   }
 end
 
-def read_from_hash(hash,"key")
+def read_from_hash(hash, key)
   
   hash = {
-    "key" => "jolie"
+    key => "jolie"
   }
-  retrun hash ["key"] 
+  retrun hash [key] 
 end
 
 def update_counting_hash(hash, key)
